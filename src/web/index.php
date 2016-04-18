c649fd1d3c8e6d3f90a7008180093c9c18abd7ce
Bernd Alter <bernd.alter@votum.de>
2016-04-18 17:14:02 +0200

    created wrapper for gui to make it accessible within vendor folder

--- src/web/index.php
+++ src/web/index.php
@@ -1,2 +1,20 @@
 <?php
-    phpinfo();
+
+$basepath = dirname(__FILE__);
+
+if (isset($_GET['file'])) {
+    $file = strval($_GET['file']);
+    $file = str_replace('../', '', $file);
+}
+
+if (empty($file)) {
+    $file = 'index.php';
+}
+
+$fullpath = sprintf($basepath . '/../vendor/generatedata/%s', $file);
+
+if (!is_file($fullpath)){
+    die('Invalid call.');
+}
+
+include_once $fullpath;
