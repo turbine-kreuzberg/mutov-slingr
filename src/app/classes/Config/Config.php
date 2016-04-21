15a74e3b181814c01745f841d3b4da098bdef93d
Christian Müllenhagen <christian.muellenhagen@votum.de>
2016-04-21 18:01:36 +0200

    [BUGFIX] updating api host after vhost update

--- src/app/classes/Config/Config.php
+++ src/app/classes/Config/Config.php
@@ -6,7 +6,7 @@ use Symfony\Component\Yaml\Parser;
 // Default Configurations, Config.yml will be merged over it
 $configDefaults = array(
   'api' => array(
-    'host' => 'http://api.mutov-slingr.votum-local.de/api/v1/data'
+    'host' => 'http://api.mutov-slingr.votum-local.de/process.php?request=data'
   ),
     'template' => array(
     'folder' => '/var/www/mutov-slingr/app/var'
